9011e95471816f299d2d2f99298d7bb9cacaae4a
tgfjt <fujita@tagpanda.co.jp>
2014-04-16 00:27:50 +0900

    init

---
+++ main.js
@@ -0,0 +1,20 @@
+(function(d) {
+
+	// 車道のホストにするDOMを取得する
+	var shadowHost = d.getElementById('talk');
+
+	if (!shadowHost.createShadowRoot) {
+		console.log('%c' + 'ShadowDomサポートしてないよ', 'color: green');
+	}
+
+	// DOMに車道のルートを作る
+	var shadowRoot = shadowHost.createShadowRoot();
+
+	// テンプレートから、車道ツリー用のDOMを取得する
+	var content = d.querySelector('#shadow').content;
+	var shadowTree = content.querySelector('#shadow-element');
+
+	// 車道ルートに車道ツリーをぶちこむ
+	shadowRoot.appendChild(shadowTree);
+
+})(document);
